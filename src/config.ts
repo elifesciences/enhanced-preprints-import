05f999af9632d42d42c083df703200269ee15884
Scott Aubrey <scott@aubrey.org.uk>
2023-01-31 15:28:06 +0000

    use more standard ENV vars that are already defined

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,8 @@ type Config = {
   eppServerUri: string,
 };
 
-if (!env.AWS_ACCESS_KEY_ID || !env.AWS_SECRET_KEY) {
-  throw Error('Environment variables `AWS_ACCESS_KEY_ID` and `AWS_SECRET_KEY` are required');
+if (!env.AWS_ACCESS_KEY_ID || !env.AWS_SECRET_ACCESS_KEY) {
+  throw Error('Environment variables `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY` are required');
 }
 
 if (!env.EPP_SERVER_URI) {
@@ -18,7 +18,7 @@ if (!env.EPP_SERVER_URI) {
 export const config: Config = {
   s3: {
     accessKey: env.AWS_ACCESS_KEY_ID,
-    secretKey: env.AWS_SECRET_KEY,
+    secretKey: env.AWS_SECRET_ACCESS_KEY,
     region: 'us-east-1',
     endPoint: 'minio',
     port: 9000,
