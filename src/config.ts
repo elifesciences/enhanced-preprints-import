cfc4b0bf06aecc21388d94898e8ff7a7b4a02f40
nlisgo <n.lisgo@elifesciences.org>
2023-04-11 14:41:18 +0100

    First pass add switching client to AWS s3 from minio

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,9 @@
-import { ClientOptions } from 'minio';
 import { env } from 'process';
+import { S3ClientConfig } from '@aws-sdk/client-s3';
 
 type Config = {
-  s3: ClientOptions,
+  s3: S3ClientConfig,
+  awsAssumeRole: S3ClientConfig,
   eppBucketName: string,
   eppServerUri: string,
   biorxivURI: string,
@@ -18,12 +19,20 @@ if (!env.EPP_SERVER_URI) {
 
 export const config: Config = {
   s3: {
-    accessKey: env.AWS_ACCESS_KEY_ID,
-    secretKey: env.AWS_SECRET_ACCESS_KEY,
+    credentials: {
+      accessKeyId: env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+    },
     region: 'us-east-1',
-    endPoint: 'minio',
-    port: 9000,
-    useSSL: false,
+    endpoint: env.S3_ENDPOINT ?? 'https://s3.amazonaws.com',
+    forcePathStyle: true,
+  },
+  awsAssumeRole: {
+    webIdentityTokenFile: process.env.AWS_WEB_IDENTITY_TOKEN_FILE ?? undefined,
+    roleArn: process.env.AWS_ROLE_ARN ?? undefined,
+    clientConfig: {
+      region: process.env.S3_REGION ?? 'us-east-1',
+    },
   },
   eppBucketName: env.BUCKET_NAME ?? 'epp',
   eppServerUri: env.EPP_SERVER_URI,
