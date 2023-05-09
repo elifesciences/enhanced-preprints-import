26d4f1f3ed2b75fc841e5fff761cfcb079afbedb
Elliot Jordan Kemp <elliotjordankemp@Elliots-MacBook-Pro.local>
2023-05-09 11:57:38 +0100

    Added initial to-do's

--- src/activities/find-all-docmaps.ts
+++ src/activities/find-all-docmaps.ts
@@ -5,8 +5,13 @@ type DocMapIndex = {
   docmaps: DocMap[],
 };
 
+// TO-DO: Replace this with find changed docmaps
 export const findAllDocmaps = async (docMapIndex: string): Promise<DocMap[] | undefined> => {
   const { data, status } = await axios.get<DocMapIndex>(docMapIndex);
+
+  // Get hashes from epp server
+
+  // Hash index and check against the epp hashes
 
   if (status === 200) {
     return data.docmaps;
