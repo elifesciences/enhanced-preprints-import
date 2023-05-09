22e1d22f90b921aa17109c46d573c1ee4cf737d6
Elliot Jordan Kemp <elliotjordankemp@Elliots-MacBook-Pro.local>
2023-05-09 12:07:40 +0100

    Added hashing for docmaps

--- src/activities/find-all-docmaps.ts
+++ src/activities/find-all-docmaps.ts
@@ -1,9 +1,12 @@
 import axios from 'axios';
 import { DocMap } from '@elifesciences/docmap-ts';
+import hash from 'object-hash';
 
 type DocMapIndex = {
   docmaps: DocMap[],
 };
+
+type IndexedHashes = { id: string, hash: string }[];
 
 // TO-DO: Replace this with find changed docmaps
 export const findAllDocmaps = async (docMapIndex: string): Promise<DocMap[] | undefined> => {
@@ -12,6 +15,10 @@ export const findAllDocmaps = async (docMapIndex: string): Promise<DocMap[] | un
   // Get hashes from epp server
 
   // Hash index and check against the epp hashes
+  const hashes: IndexedHashes = data.docmaps.map((docMap) => ({ id: docMap.id, hash: hash(docMap) }));
+
+  // eslint-disable-next-line
+  hashes.forEach((entry) => console.log('Hashes', entry));
 
   if (status === 200) {
     return data.docmaps;
