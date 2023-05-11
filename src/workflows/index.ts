f6db9c9fde6c82678d04a368f9ee08cb0018f24a
Elliot Jordan Kemp <elliotjordankemp@Elliots-MacBook-Pro.local>
2023-05-11 16:11:36 +0100

    Added untracked change

--- src/workflows/index.ts
+++ src/workflows/index.ts
@@ -1,4 +1,4 @@
 export { importDocmaps } from './import-docmaps';
 export { importDocmap } from './import-docmap';
 export { importContent } from './import-content';
-export { pollDocMapIndex } from './loop-timer';
+export { pollDocMapIndex } from './poll-docmap-index';
