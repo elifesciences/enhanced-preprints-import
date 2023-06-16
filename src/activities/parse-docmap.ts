13e3831814495b47b26563912a62ac13a895d5cb
nlisgo <n.lisgo@elifesciences.org>
2023-06-16 17:37:29 +0100

    Revert "Refactor parse-docmap.ts to match versions with tdmPaths"
    This reverts commit 059c6efc258628532d6811025d99900f097ce10c.

--- src/activities/parse-docmap.ts
+++ src/activities/parse-docmap.ts
@@ -7,18 +7,16 @@ export const parseDocMap = async (docMapInput: string): Promise<parser.Manuscrip
     // TO-DO: This is a temporary hack, remove/replace once DocMaps are fixed to hold the meca path in the output content
     const { steps } = JSON.parse(docMapInput);
 
-    const contentPaths: Map<string, string> = new Map();
+    const contentPaths: string[] = [];
     Object.keys(steps).forEach((key) => {
       // Loop through the actions in a step and the corresponding outputs to extract _tdmPaths
       // eslint-disable-next-line @typescript-eslint/dot-notation
-      steps[key].actions.forEach((action: any) => action.outputs?.forEach((output: any) => { if (output['_tdmPath'] && output['versionIdentifier']) contentPaths.set(output['versionIdentifier'], output['_tdmPath']); }));
+      steps[key].actions.forEach((action: any) => action.outputs?.forEach((output: any) => { if (output['_tdmPath']) contentPaths.push(output['_tdmPath']); }));
     });
 
-    if (manuscriptData && manuscriptData.versions.length >= contentPaths.size) {
-      manuscriptData.versions = manuscriptData.versions
-        .filter((version) => contentPaths.has(version.versionIdentifier))
-        .map<VersionedReviewedPreprint>((version) => ({ ...version, preprint: { ...version.preprint, content: contentPaths.get(version.versionIdentifier) } }));
-    } else throw Error(`Number of versions (${manuscriptData?.versions.length}) from parser does not match or exceed the number of tdmPaths in the DocMap (${contentPaths.size})`);
+    if (contentPaths.length === manuscriptData?.versions.length) {
+      manuscriptData.versions = manuscriptData.versions.map<VersionedReviewedPreprint>((version, index) => ({ ...version, preprint: { ...version.preprint, content: contentPaths[index] } }));
+    } else throw Error(`Number of versions (${manuscriptData?.versions.length}) from parser does not match the number of tdmPaths in the DocMap (${contentPaths.length})`);
 
     return manuscriptData;
   } catch (error: any) {
