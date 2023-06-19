e007ca716fef1b3bc225757874a2d8b4af866e2d
nlisgo <n.lisgo@elifesciences.org>
2023-06-19 15:27:55 +0100

    Merge in upstream changes

--- src/S3Bucket.ts
+++ src/S3Bucket.ts
@@ -2,13 +2,13 @@ import { VersionedReviewedPreprint } from '@elifesciences/docmap-ts';
 import { readFileSync } from 'fs';
 import { S3Client } from '@aws-sdk/client-s3';
 import { fromWebToken, fromTemporaryCredentials } from '@aws-sdk/credential-providers';
-import { config } from './config';
+import { AwsAssumeRole, S3, config } from './config';
 
-const getAWSCredentials = () => {
-  if (config.awsAssumeRole.webIdentityTokenFile !== undefined && config.awsAssumeRole.roleArn !== undefined) {
-    const webIdentityToken = readFileSync(config.awsAssumeRole.webIdentityTokenFile, 'utf-8');
+const getAWSCredentials = (awsAssumeRole: AwsAssumeRole, s3: S3) => {
+  if (awsAssumeRole.webIdentityTokenFile !== undefined && awsAssumeRole.roleArn !== undefined) {
+    const webIdentityToken = readFileSync(awsAssumeRole.webIdentityTokenFile, 'utf-8');
     return fromWebToken({
-      roleArn: config.awsAssumeRole.roleArn,
+      roleArn: awsAssumeRole.roleArn,
       clientConfig: {
         region: config.s3.region,
       },
@@ -18,15 +18,15 @@ const getAWSCredentials = () => {
   if (config.awsAssumeRole.roleArn !== undefined) {
     return fromTemporaryCredentials({
       params: {
-        RoleArn: config.awsAssumeRole.roleArn,
+        RoleArn: awsAssumeRole.roleArn,
         DurationSeconds: 900,
       },
       clientConfig: {
         credentials: {
-          accessKeyId: config.s3.accessKey ?? '',
-          secretAccessKey: config.s3.secretKey ?? '',
+          accessKeyId: s3.accessKey ?? '',
+          secretAccessKey: s3.secretKey ?? '',
         },
-        region: config.s3.region,
+        region: s3.region,
       },
     });
   }
@@ -36,12 +36,16 @@ const getAWSCredentials = () => {
   };
 };
 
-export const getS3Client = () => new S3Client({
-  credentials: getAWSCredentials(),
-  endpoint: config.s3.endPoint,
+const preprareS3Client = (awsAssumeRole: AwsAssumeRole, s3: S3) => new S3Client({
+  credentials: getAWSCredentials(awsAssumeRole, s3),
+  endpoint: s3.endPoint,
   forcePathStyle: true,
-  region: config.s3.region,
+  region: s3.region,
 });
+
+export const getS3Client = () => preprareS3Client(config.awsAssumeRole, config.s3);
+export const getMecaS3Client = () => preprareS3Client(config.mecaAwsAssumeRole, config.mecaS3);
+export const sharedS3 = (): boolean => (config.s3.endPoint === config.mecaS3.endPoint);
 
 export const parseS3Path = (s3Path: string): S3File => {
   const url = new URL(s3Path);
