b36a582131d08778eb0604c27e97d11b88a0194a
Elliot Jordan Kemp <elliotjordankemp@Elliots-MacBook-Pro.local>
2023-06-21 11:39:40 +0100

    Changed parse-docmap to reduce versions based on content paths and adjusted test

--- src/activities/parse-docmap.ts
+++ src/activities/parse-docmap.ts
@@ -6,21 +6,24 @@ export const parseDocMap = async (docMapInput: string): Promise<parser.Manuscrip
   const manuscriptData = parser.parsePreprintDocMap(docMapInput);
 
   try {
+    if (!manuscriptData) { throw new Error('Manuscript data is undefined'); }
+
     // TO-DO: This is a temporary hack, remove/replace once DocMaps are fixed to hold the meca path in the output content
     const { steps } = JSON.parse(docMapInput);
 
-    const contentPaths: string[] = [];
+    const contentPaths: Record<string, string> = {};
     Object.keys(steps).forEach((key) => {
       // Loop through the actions in a step and the corresponding outputs to extract _tdmPaths
       // eslint-disable-next-line @typescript-eslint/dot-notation
-      steps[key].actions.forEach((action: any) => action.outputs?.forEach((output: any) => { if (output['_tdmPath']) contentPaths.push(output['_tdmPath']); }));
+      steps[key].actions.forEach((action: any) => action.outputs?.forEach((output: any) => { if (output['_tdmPath']) contentPaths[output.versionIdentifier] = output['_tdmPath']; }));
     });
 
-    // where manuscriptData?.versions.length is 1 and contentPaths.length is 0, we get an error (i.e. when there is a preprint submitted without a published version)
-    if (manuscriptData && contentPaths.length === 0) return manuscriptData;
-    if (contentPaths.length === manuscriptData?.versions.length) {
-      manuscriptData.versions = manuscriptData.versions.map<VersionedReviewedPreprint>((version, index) => ({ ...version, preprint: { ...version.preprint, content: contentPaths[index] } }));
-    } else throw Error(`Number of tdmPaths in the DocMap (${manuscriptData?.versions.length}) from parser does not match the number of versions (${contentPaths.length})`);
+    manuscriptData.versions = manuscriptData.versions.reduce<VersionedReviewedPreprint[]>((versions, version) => {
+      if (contentPaths[version.versionIdentifier]) {
+        versions.push({ ...version, preprint: { ...version.preprint, content: contentPaths[version.versionIdentifier] } });
+      }
+      return versions;
+    }, []);
 
     return manuscriptData;
   } catch (error: any) {
