7504aa5b4e47214d59f55cca73f358e94db4f576
Scott Aubrey <scott@aubrey.org.uk>
2023-09-21 08:25:16 +0100

    remove heartbeat timeout and extend startToClose timeout

--- src/workflows/import-content.ts
+++ src/workflows/import-content.ts
@@ -33,8 +33,7 @@ const {
 const {
   extractMeca,
 } = proxyActivities<typeof activities>({
-  startToCloseTimeout: '15 minutes',
-  heartbeatTimeout: '5 minutes',
+  startToCloseTimeout: '20 minutes',
   retry: {
     initialInterval: '1 minute',
     backoffCoefficient: 2,
