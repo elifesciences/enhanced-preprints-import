65b52abc93111251a579c84635990ec84697b8cd
Scott Aubrey <scott@aubrey.org.uk>
2023-12-29 10:53:24 +0000

    add todo tests

---
+++ src/workflows/import-docmaps.test.ts
@@ -0,0 +1,6 @@
+describe('importDocmaps', () => {
+  it.todo('should create importdocmap workflows if the number of docmap changes are below a threshold');
+  it.todo('should not create importdocmap workflows if the number of docmap changes are above a threshold');
+  it.todo('should progress importdocmap workflows if it gets an approval signal');
+  it.todo('should not progress importdocmap workflows if it gets a rejection signal');
+});
