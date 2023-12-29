334b5b7f6327b32a95b1fa4555d5588d139a59e0
Scott Aubrey <scott@aubrey.org.uk>
2023-12-29 11:14:26 +0000

    create a test environment for workflow testing

--- src/workflows/import-docmaps.test.ts
+++ src/workflows/import-docmaps.test.ts
@@ -1,4 +1,16 @@
+import { TestWorkflowEnvironment } from '@temporalio/testing';
+
 describe('importDocmaps', () => {
+  let testEnv: TestWorkflowEnvironment;
+
+  beforeAll(async () => {
+    testEnv = await TestWorkflowEnvironment.createTimeSkipping();
+  });
+
+  afterAll(async () => {
+    await testEnv?.teardown();
+  });
+
   it.todo('should create importdocmap workflows if the number of docmap changes are below a threshold');
   it.todo('should not create importdocmap workflows if the number of docmap changes are above a threshold');
   it.todo('should progress importdocmap workflows if it gets an approval signal');
