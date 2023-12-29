7f1abab77acf8ad0a5cdbfe867ddc66bce20c1f5
Aisha <a.kapan@elifesciences.org>
2023-12-29 12:59:16 +0000

    Add failing tests with wrong input for different threshold cases

--- src/workflows/import-docmaps.test.ts
+++ src/workflows/import-docmaps.test.ts
@@ -32,8 +32,44 @@ describe('importDocmaps', () => {
     );
     expect(result.status).toEqual('SKIPPED');
   });
-  it.todo('should create importdocmap workflows if the number of docmap changes are below a threshold'); 
-  it.todo('should not create importdocmap workflows if the number of docmap changes are above a threshold');
+  it('should create importdocmap workflows if the number of docmap changes are below a threshold', async() => {
+    const worker = await Worker.create({
+      connection: testEnv.nativeConnection,
+      taskQueue: 'test-epp',
+      workflowsPath: require.resolve('./'),
+      activities: {
+        filterDocmapIndex: () => [],
+        mergeDocmapState: () => false,
+      }
+    });
+    const result = await worker.runUntil(
+      testEnv.client.workflow.execute(importDocmaps, {
+        workflowId: 'import-docmaps',
+        taskQueue: 'test-epp',
+        args: ['http://test-docmaps.com']
+      })
+    );
+    expect(result.status).toEqual('SUCCESS');
+  }); 
+  it('should not create importdocmap workflows if the number of docmap changes are above a threshold', async() => {
+    const worker = await Worker.create({
+      connection: testEnv.nativeConnection,
+      taskQueue: 'test-epp',
+      workflowsPath: require.resolve('./'),
+      activities: {
+        filterDocmapIndex: () => [],
+        mergeDocmapState: () => false,
+      }
+    });
+    const result = await worker.runUntil(
+      testEnv.client.workflow.execute(importDocmaps, {
+        workflowId: 'import-docmaps',
+        taskQueue: 'test-epp',
+        args: ['http://test-docmaps.com']
+      })
+    );
+    expect(result.status).toEqual('NOT APPROVED');
+  }); 
   it.todo('should progress importdocmap workflows if it gets an approval signal');
   it.todo('should not progress importdocmap workflows if it gets a rejection signal');
 });
