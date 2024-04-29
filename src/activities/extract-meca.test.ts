40d6f8420f022f5e602fcc1b9d7eeee6f0396826
A. K <74007089+soggy-mushroom@users.noreply.github.com>
2024-04-29 15:52:30 +0100

    [#1041]: Add s3 client mock and temporal context to extractMeca test

--- src/activities/extract-meca.test.ts
+++ src/activities/extract-meca.test.ts
@@ -1,12 +1,37 @@
-import extractedMecaResult from '../test-utils/extracted-meca-file.json';
+import { mockClient } from 'aws-sdk-client-mock';
+import {
+  GetObjectCommand, S3Client,
+} from '@aws-sdk/client-s3';
+import { sdkStreamMixin } from '@aws-sdk/util-stream-node';
+import { createReadStream } from 'fs';
 import { extractMeca } from './extract-meca';
+import extractedMecaResult from '../test-utils/extracted-meca-file.json';
 
 jest.mock('../config', () => ({
-  config: {},
+  config: {
+    eppS3: { webIdentityTokenFile: '' },
+
+  },
 }));
+
+jest.mock('@temporalio/activity', () => ({
+  Context: {
+    current: () => ({
+      heartbeat: () => {},
+    }),
+  },
+}));
+
+const mockS3Client = mockClient(S3Client);
 
 describe('extract-meca-activity', () => {
   it.skip('returns extracted meca file', async () => {
+    const stream = createReadStream('./mock-data/meca/dummy-1.meca');
+    const sdkStream = sdkStreamMixin(stream);
+    mockS3Client.on(GetObjectCommand).resolves({
+      Body: sdkStream,
+    });
+
     const version = {
       id: 'id1',
       versionIdentifier: 'ver1',
