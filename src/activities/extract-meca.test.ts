35624c7fb29f05127f0e8db8c7f744f8cd060522
Scott Aubrey <scott@aubrey.org.uk>
2024-05-01 15:42:15 +0100

    [enhanced-preprints-issues/issues#1041]: assert correct files were uploaded to S3 and the content for the XML file contains the article tag

--- src/activities/extract-meca.test.ts
+++ src/activities/extract-meca.test.ts
@@ -1,16 +1,17 @@
 import { mockClient } from 'aws-sdk-client-mock';
 import {
-  GetObjectCommand, S3Client,
+  GetObjectCommand, PutObjectCommand, PutObjectCommandInput, S3Client,
 } from '@aws-sdk/client-s3';
 import { sdkStreamMixin } from '@aws-sdk/util-stream-node';
-import { createReadStream } from 'fs';
+import { ReadStream, createReadStream } from 'fs';
 import { extractMeca } from './extract-meca';
 import extractedMecaResult from '../test-utils/extracted-meca-file.json';
 
 jest.mock('../config', () => ({
   config: {
     eppS3: { webIdentityTokenFile: '' },
-
+    eppBucketName: 'test-bucket', // This is the default bucket name to store files in S3
+    eppBucketPrefix: 'automation/', // This is the default prefix to give to files in S3
   },
 }));
 
@@ -24,12 +25,30 @@ jest.mock('@temporalio/activity', () => ({
 
 const mockS3Client = mockClient(S3Client);
 
+// utility function from https://gist.github.com/lambrospetrou/0c9ac9da14d8d241ae3634981ceb2871
+function streamToString(stream: ReadStream): Promise<string> {
+  const chunks: Array<any> = [];
+  return new Promise((resolve, reject) => {
+    stream.on('data', (chunk) => chunks.push(chunk));
+    stream.on('error', reject);
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+  });
+}
+
 describe('extract-meca-activity', () => {
   it('returns extracted meca file', async () => {
     const stream = createReadStream('./mock-data/meca/dummy-1.meca');
     const sdkStream = sdkStreamMixin(stream);
     mockS3Client.on(GetObjectCommand).resolves({
       Body: sdkStream,
+    });
+
+    // setup a fake receiver for PutObjectCommand calls
+    const uploadedFiles: { [key: string]: string } = {};
+    mockS3Client.on(PutObjectCommand).callsFake(async (input: PutObjectCommandInput) => {
+      if (typeof input.Key === 'string' && input.Body) {
+        uploadedFiles[input.Key] = await streamToString(input.Body as ReadStream);
+      }
     });
 
     const version = {
@@ -44,5 +63,26 @@ describe('extract-meca-activity', () => {
     const result = await extractMeca(version);
 
     expect(result).toMatchObject(extractedMecaResult);
+
+    expect(Object.keys(uploadedFiles).sort()).toEqual([
+      'automation/id1/vver1/2212.00741/2212.00741.xml',
+      'automation/id1/vver1/2212.00741/2212.00741v1_fig1.jpg',
+      'automation/id1/vver1/2212.00741/2212.00741v1_fig2.jpg',
+      'automation/id1/vver1/2212.00741/2212.00741v1_fig3.jpg',
+      'automation/id1/vver1/2212.00741/2212.00741v1_fig4.jpg',
+      'automation/id1/vver1/2212.00741/2212.00741v1_fig5.jpg',
+      'automation/id1/vver1/2212.00741/2212.00741v1_fig6.jpg',
+      'automation/id1/vver1/2212.00741/2212.00741v1_ieqn1.jpg',
+      'automation/id1/vver1/2212.00741/2212.00741v1_tbl1a.jpg',
+      'automation/id1/vver1/2212.00741/2212.00741v1_tbl1b.jpg',
+      'automation/id1/vver1/2212.00741/2212.00741v1_tbl1c.jpg',
+      'automation/id1/vver1/2212.00741/2212.00741v1_tbl1d.jpg',
+      'automation/id1/vver1/2212.00741/2212.00741v1_tbl2a.jpg',
+      'automation/id1/vver1/2212.00741/2212.00741v1_tbl2b.jpg',
+      'automation/id1/vver1/2212.00741/2212.00741v1_tbl2c.jpg',
+      'automation/id1/vver1/2212.00741/2212.00741v1_tbl2d.jpg',
+      'automation/id1/vver1/2212.00741/2212.00741v1_tbl2e.jpg',
+    ]);
+    expect(uploadedFiles['automation/id1/vver1/2212.00741/2212.00741.xml']).toContain('<article ');
   });
 });
