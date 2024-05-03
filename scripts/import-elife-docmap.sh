1fae4bd8b26c6dd297cbf77f10f01c13e1385ea4
Nathan Lisgo <nathan@lisgo.co.uk>
2024-05-03 13:37:57 +0100

    consolidate import-elife-docmap scripts

--- scripts/import-elife-docmap.sh
+++ scripts/import-elife-docmap.sh
@@ -9,12 +9,22 @@
 ## 2. (optionally) export env vars TEMPORAL_ADDRESS to point to the address for the temporal GRPC service (e.g. localhost:7233)
 ## 3. run `./scripts/import-elife-docmap.sh {ManuscriptId}`
 ##
-## Optionally, you can also previde a non-default workflow ID prefix as the second parameter. The docmap has will be appended to this string
+## Optionally, you can also provide a non-default workflow ID prefix as the second parameter. The docmap has will be appended to this string
 ## `./scripts/import-elife-docmap.sh "12345" "manual-import-2024-02-05-"`
+##
+## If the second parameter is epp--prod or epp--staging, it will be used as the namespace and the workflow ID will be the 3rd parameter
+## `./scripts/import-elife-docmap.sh "12345" "epp--staging"` or
+## `./scripts/import-elife-docmap.sh "12345" "epp--staging" "manual-import-2024-02-05-"`
 
 docmap_api_manuscript_prefix=https://data-hub-api.elifesciences.org/enhanced-preprints/docmaps/v2/by-publisher/elife/get-by-manuscript-id?manuscript_id=
 
-workflow_id_prefix="${2:-"docmap-"}"
+namespace=${2:-"epp--prod"}
+workflow_id_prefix=${3:-"docmap-"}
+
+if [[ "$namespace" != "epp--prod" && "$namespace" != "epp--staging" ]]; then
+  workflow_id_prefix="$namespace"
+  namespace="epp--prod"
+fi
 
 if [ -z "$1" ]; then
   echo "No manuscript-id supplied"
@@ -40,4 +50,4 @@ else
     exit 3
 fi
 
-temporal workflow start -n epp--prod --type importDocmap  -i '"'$docmap_url'"' -t 'epp' -w "$workflow_id_prefix$docmap_id_hash" --id-reuse-policy=TerminateIfRunning
+temporal workflow start -n "$namespace" --type importDocmap  -i '"'$docmap_url'"' -t 'epp' -w "$workflow_id_prefix$docmap_id_hash" --id-reuse-policy=TerminateIfRunning
