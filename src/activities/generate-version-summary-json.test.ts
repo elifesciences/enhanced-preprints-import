2795efe819453de824bc3a87262b736b7322ee56
Will Byrne <will.alex.byrne@gmail.com>
2024-07-01 15:08:30 +0100

    add testlist for generateVersionSummaryJson [elifesciences/enhanced-preprints-issues#1122]

---
+++ src/activities/generate-version-summary-json.test.ts
@@ -0,0 +1,12 @@
+describe('generate-version-summary-json', () => {
+  describe('happy path', () => {
+    it.todo('generates a version summary');
+    it.todo('generates a version summary with corrections');
+    it.todo('emmits the correct heartbeats');
+  });
+
+  describe('error path', () => {
+    it.todo('throws NonRetryableError if there is no published date');
+    it.todo('throws NonRetryableError if there is no content url');
+  });
+});
