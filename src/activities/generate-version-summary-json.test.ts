f7203317007ca1de85832635106ab828da7105a0
Nathan Lisgo <nathan@lisgo.co.uk>
2024-07-01 17:03:47 +0100

    Ensure generation of version summary json [elifesciences/enhanced-preprints-issues#1122]

--- src/activities/generate-version-summary-json.test.ts
+++ src/activities/generate-version-summary-json.test.ts
@@ -1,6 +1,60 @@
+import { mockClient } from 'aws-sdk-client-mock';
+import { PutObjectCommand, PutObjectCommandInput, S3Client } from '@aws-sdk/client-s3';
+import { generateVersionSummaryJson } from '.';
+
+jest.mock('../config', () => ({
+  config: {
+    eppS3: { webIdentityTokenFile: '' },
+    eppBucketName: 'test-bucket', // This is the default bucket name to store files in S3
+    eppBucketPrefix: 'automation/', // This is the default prefix to give to files in S3
+  },
+}));
+
+jest.mock('@temporalio/activity', () => ({
+  Context: {
+    current: () => ({
+      heartbeat: () => {},
+    }),
+  },
+}));
+
+const mockS3Client = mockClient(S3Client);
+
 describe('generate-version-summary-json', () => {
   describe('happy path', () => {
-    it.todo('generates a version summary');
+    it('generates a version summary', async () => {
+      const data = {
+        msid: 'msid',
+        version: {
+          versionIdentifier: 'versionIdentifier',
+          id: 'id',
+          doi: 'doi',
+          publishedDate: new Date('01-07-2024'),
+          content: ['http://content'],
+        },
+      };
+
+      // setup a fake receiver for PutObjectCommand calls
+      const uploadedFiles: { [key: string]: any } = {};
+      mockS3Client.on(PutObjectCommand).callsFake(async (input: PutObjectCommandInput) => {
+        if (typeof input.Key === 'string' && input.Body) {
+          uploadedFiles[input.Key] = JSON.parse(input.Body as string);
+        }
+      });
+
+      await generateVersionSummaryJson(data);
+      expect(uploadedFiles).toEqual({
+        'automation/id/vversionIdentifier/payload.json': {
+          doi: 'doi',
+          id: 'idvversionIdentifier',
+          msid: 'msid',
+          published: '2024-01-07T00:00:00.000Z',
+          url: 'http://content',
+          versionIdentifier: 'versionIdentifier',
+        },
+      });
+    });
+
     it.todo('generates a version summary with corrections');
     it.todo('emmits the correct heartbeats');
   });
